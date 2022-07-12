b0e1ff6593e9f89118705b87d321d76f731245d4
hyemmmmgit <108560213+hyemmmmgit@users.noreply.github.com>
2022-07-12 15:30:43 +0900

    220712

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,4 +1,4 @@
-const sectionList = document.querySelectorAll('.projects section');
+const sectionList = document.querySelectorAll('.projects section .FirstLine');
 const descList = document.querySelectorAll('.projects section .desc');
 const arrowDownList = document.querySelectorAll('.FirstLine .fa-angle-down');
 const arrowUpList = document.querySelectorAll('.FirstLine .fa-angle-up');
@@ -22,5 +22,34 @@ headerMenuList[1].onclick = function () {
   scrollTo(0, 600);
 };
 headerMenuList[2].onclick = function () {
-  scrollTo(0, 1266);
+  scrollTo(0, 1350);
 };
+
+const projects = document.querySelectorAll('section');
+
+for (let i = 0; i < projects.length; i++) {
+  let mainImg = projects[i].querySelector('.imgBox img');
+  let smallPics = projects[i].querySelectorAll('.smallImgBox img');
+  let smallPicsDiv = projects[i].querySelectorAll('.smallImgBox div');
+
+  for (let i = 0; i < smallPics.length; i++) {
+    smallPics[i].addEventListener('click', changePic);
+  }
+
+  function changePic() {
+    let newPic = this.src;
+    mainImg.setAttribute('src', newPic);
+  }
+
+  for (let i = 0; i < smallPicsDiv.length; i++) {
+    smallPicsDiv[i].addEventListener('click', function () {
+      if (i === 0) {
+        smallPicsDiv[0].classList.add('selected');
+        smallPicsDiv[1].classList.remove('selected');
+      } else {
+        smallPicsDiv[1].classList.add('selected');
+        smallPicsDiv[0].classList.remove('selected');
+      }
+    });
+  }
+}
